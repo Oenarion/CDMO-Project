ce8b672b09976b8c7fa8044567e9e22afb71482d
Giacomo <giacomo.caroli@live.it>
2023-05-02 16:21:28 +0200

    add evaluation of tours matrix with parser

--- SAT/main.py
+++ SAT/main.py
@@ -1,50 +1,13 @@
+import numpy as np
 from z3 import *
 from itertools import *
 import math
 
-def find(index,tours):
-    """
-    per ogni [i][j] di tours, abbiamo una lista k1,k2,k3,...kn
-    questa lista viene messa in AND logico dentro una parentesi (k1 & k2 & k3 ...)
-    ogni k1 .. ha davanti l'operatore logico corrispondente per computare l'elemento index
-    es: index = 2 bin: 0010 (meno significativo a dx)
-    --> cerchiamo un numero (!k1 & !k2 & k3 & !k4)
-    tutte vengono aggiunte alla list[]
-    si controlla che in list sia presente exactly_once(list[])
-    quindi in tutte le i,j combinazioni di tours, solo una deve "accendersi" logicamente con l'index corrispondente
-    """
-
-    list = []
-    enc = bin(index) #codifica in binario di index (1..n) -> 0b10 (esempio per index = 2)
-    index_encoding = enc[2:].rjust(depth_tours, '0') #portiamo con padding depth_tours (ex: 8) -> '00001000'
-
-    #recuperiamo tutti i k elementi di un [i][j]
-    #j in range(n-m+3)] for i in range(m)]
-    for i in range(m):
-        for j in range(n-m+3):
-            #abbiamo tutti i k di depth associati a [i][j]-esimo
-            sub_list = []
-            for k in range(depth_tours):
-                #k0,k1,k2... <--> index_encoding (stessa dimensione)
-                if(index_encoding[k] == '0'):
-                    sub_list.append(Not(tours[i][j][k]))
-                else:
-                    sub_list.append(tours[i][j][k])
-
-            #questa sub_lista deve avere tutti gli elementi in AND logico
-            #successivamente vanno aggiunti alla list[] in OR
-            list.append(And(sub_list))
-
-    return list
-
-
 def at_least_one(bool_vars):
-
     return Or(bool_vars)
 
 def at_most_one(bool_vars):
     return [Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)]
-
 
 def exactly_one(bool_vars):
     return at_most_one(bool_vars) + [at_least_one(bool_vars)]
@@ -82,15 +45,113 @@ items = [[Bool(f"item{i}_{j}") for i in range(n)] for j in range(depth_weight)]
 #items index = {i} index from items list
 #we should add a constraint that says that from all i,j
 
-#def graphic_representation():
-    
-
 s = Solver() # create a solver s
+
+def find(index,tours):
+    """
+    per ogni [i][j] di tours, abbiamo una lista k1,k2,k3,...kn
+    questa lista viene messa in AND logico dentro una parentesi (k1 & k2 & k3 ...)
+    ogni k1 .. ha davanti l'operatore logico corrispondente per computare l'elemento index
+    es: index = 2 bin: 0010 (meno significativo a dx)
+    --> cerchiamo un numero (!k1 & !k2 & k3 & !k4)
+    tutte vengono aggiunte alla list[]
+    si controlla che in list sia presente exactly_once(list[])
+    quindi in tutte le i,j combinazioni di tours, solo una deve "accendersi" logicamente con l'index corrispondente
+    """
+
+    list = []
+    enc = bin(index) #codifica in binario di index (1..n) -> 0b10 (esempio per index = 2)
+    index_encoding = enc[2:].rjust(depth_tours, '0') #portiamo con padding depth_tours (ex: 8) -> '00001000'
+
+    #recuperiamo tutti i k elementi di un [i][j]
+    #j in range(n-m+3)] for i in range(m)]
+    for i in range(m):
+        for j in range(n-m+3):
+            #abbiamo tutti i k di depth associati a [i][j]-esimo
+            sub_list = []
+            for k in range(depth_tours):
+                #k0,k1,k2... <--> index_encoding (stessa dimensione)
+                if(index_encoding[k] == '0'):
+                    sub_list.append(Not(tours[i][j][k]))
+                else:
+                    sub_list.append(tours[i][j][k])
+
+            #questa sub_lista deve avere tutti gli elementi in AND logico
+            #successivamente vanno aggiunti alla list[] in OR
+            list.append(And(sub_list))
+
+    return list
 
 #constraint 1: each item exactly once
 for i in range(1,n+1): #iterate on 1,2,3...5
     s.add(exactly_one(find(i,tours))) #for each [i][j] where bin(item_index) == [j][j][:] --> add constraint exactly_once
 
+
 print(s.check())
 
-print(s.model())
+model = s.model()
+
+
+
+
+
+
+
+#faccio una lista di tuple
+results = []
+
+# itera attraverso le variabili del modello
+for decl in model:
+    # ottieni il nome della variabile
+    name = decl.name()
+
+    #rimuovo tour dal nome
+    sub_string = name[4:]
+    parts = sub_string.split("_")
+
+    # ottieni il valore assegnato alla variabile nel modello
+    val = model[decl]
+
+    #i,j,k
+    results.append((int(parts[0]),int(parts[1]),int(parts[2]), bool(val)))
+    # stampa il nome e il valore della variabile
+    #print(f"{name}: {val}")
+
+
+matrix = [[['0' for x in range(depth_tours)] for y in range(n-m+3)] for z in range(m)]
+for r in results:
+    if r[3]:
+        matrix[r[0]][r[1]][r[2]] = '1'
+    else:
+        matrix[r[0]][r[1]][r[2]] = '0'
+
+matrix2 = [['0' for y in range(n-m+3)] for z in range(m)]
+for i in range(m):
+    for j in range(n-m+3):
+
+        string_bin = ""
+
+        for k in range(depth_tours):
+            string_bin += matrix[i][j][k]
+
+        #conversione in dec
+        matrix2[i][j] = int (string_bin,2)
+
+for row in matrix2:
+    print(row)
+
+#costruisco la matrice
+
+
+"""
+#tours = [[[Bool(f"tour{i}_{j}_{k}") for k in range(depth_tours)] for j in range(n-m+3)] for i in range(m)]
+matrix = [[[False for _ in range(depth_tours)] for _ in range(n-m+3)] for _ in range(m)]
+
+for var_name, value in model:
+    if var_name.startswith("tour"):
+        parts = var_name.split("_")
+        row = int(parts[1])
+        col = int(parts[2])
+        layer = int(parts[3])
+        matrix[row][col][layer] = value
+"""
