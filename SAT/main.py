cc13c084866f4dfedf7e359cb3cb4471954db601
Luca3700 <luca.torzi37@gmail.com>
2023-05-12 17:40:59 +0200

    added distance evaluation

--- SAT/main.py
+++ SAT/main.py
@@ -2,14 +2,59 @@ from z3 import *
 from itertools import *
 import math
 
-def at_least_one(bool_vars):
+def at_least_one_np(bool_vars):
     return Or(bool_vars)
+def at_most_one_np(bool_vars, name = ""):
+    return And([Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)])
+def exactly_one_np(bool_vars, name = ""):
+    return And(at_least_one_np(bool_vars), at_most_one_np(bool_vars, name))
 
-def at_most_one(bool_vars):
-    return [Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)]
 
-def exactly_one(bool_vars):
-    return at_most_one(bool_vars) + [at_least_one(bool_vars)]
+def at_least_one_seq(bool_vars):
+    return at_least_one_np(bool_vars)
+def at_most_one_seq(bool_vars, name):
+    constraints = []
+    n = len(bool_vars)
+    s = [Bool(f"s_{name}_{i}") for i in range(n - 1)]
+    constraints.append(Or(Not(bool_vars[0]), s[0]))
+    constraints.append(Or(Not(bool_vars[n-1]), Not(s[n-2])))
+    for i in range(1, n - 1):
+        constraints.append(Or(Not(bool_vars[i]), s[i]))
+        constraints.append(Or(Not(bool_vars[i]), Not(s[i-1])))
+        constraints.append(Or(Not(s[i-1]), s[i]))
+    return And(constraints)
+def exactly_one_seq(bool_vars, name):
+    return And(at_least_one_seq(bool_vars), at_most_one_seq(bool_vars, name))
+
+def at_least_one_he(bool_vars):
+    return at_least_one_np(bool_vars)
+def at_most_one_he(bool_vars, name):
+    if len(bool_vars) <= 4:
+        return And(at_most_one_np(bool_vars))
+    y = Bool(f"y_{name}")
+    return And(And(at_most_one_np(bool_vars[:3] + [y])), And(at_most_one_he(bool_vars[3:] + [Not(y)], name+"_")))
+def exactly_one_he(bool_vars, name):
+    return And(at_most_one_he(bool_vars, name), at_least_one_he(bool_vars))
+
+def at_least_k_seq(bool_vars, k, name):
+    return at_most_k_seq([Not(var) for var in bool_vars], len(bool_vars)-k, name)
+def at_most_k_seq(bool_vars, k, name):
+    constraints = []
+    n = len(bool_vars)
+    s = [[Bool(f"s_{name}_{i}_{j}") for j in range(k)] for i in range(n - 1)]
+    constraints.append(Or(Not(bool_vars[0]), s[0][0]))
+    constraints += [Not(s[0][j]) for j in range(1, k)]
+    for i in range(1, n-1):
+        constraints.append(Or(Not(bool_vars[i]), s[i][0]))
+        constraints.append(Or(Not(s[i-1][0]), s[i][0]))
+        constraints.append(Or(Not(bool_vars[i]), Not(s[i-1][k-1])))
+        for j in range(1, k):
+            constraints.append(Or(Not(bool_vars[i]), Not(s[i-1][j-1]), s[i][j]))
+            constraints.append(Or(Not(s[i-1][j]), s[i][j]))
+    constraints.append(Or(Not(bool_vars[n-1]), Not(s[n-2][k-1])))   
+    return And(constraints)
+def exactly_k_seq(bool_vars, k, name):
+    return And(at_most_k_seq(bool_vars, k, name), at_least_k_seq(bool_vars, k, name))
 
 #input model
 m = 3 #couriers
@@ -24,6 +69,44 @@ D = [[0, 3, 3, 6, 5, 6, 6, 2], #distances
     [6, 7, 3, 6, 3, 0, 2, 4],
     [6, 7, 5, 6, 3, 2, 0, 4],
     [2, 3, 3, 4, 3, 4, 4, 0]]
+
+# m = 5
+# n = 20
+# l = [15, 43, 42, 16, 42]
+# s_dato = [5, 4, 3, 5, 6, 3, 9, 5, 6, 1, 6, 4, 8, 3, 1, 4, 8, 6, 8, 8]
+# D = [  [0, 10,  5,  2, 11,  7, 20, 16,  6,  1, 18, 20, 15, 19, 17, 10, 18, 19,  6, 13, 10],
+#   [20,  0, 16, 10,  7, 16, 17,  9, 16, 11,  9, 15, 13,  9, 12, 13,  8, 14,  9,  6,  6],
+#   [19, 20,  0, 15, 11,  7, 18, 12, 16, 10, 19, 15, 11, 20, 18, 10, 15, 16,  7, 19, 12],
+#   [17, 16, 11,  0, 19,  8, 19, 20, 14, 18, 17, 18, 16, 20, 19, 14, 20, 20, 11, 20, 17],
+#   [13, 11, 12, 11,  0, 16, 20, 12, 17, 14,  9, 17, 18, 15, 11,  9, 15, 19, 19, 16, 14],
+#   [17, 13,  7,  9, 16,  0, 20, 13, 14, 12, 19, 19, 11, 13, 16, 11, 14, 20,  7, 12, 16],
+#   [10,  9, 13, 10, 11, 15,  0, 10,  9,  8, 10,  7,  7,  8,  5, 11,  6, 11,  6, 13,  9],
+#   [15,  8, 14, 15,  4, 20, 14,  0, 19, 15,  8, 14, 17, 16, 11, 12,  8, 20, 10, 12, 14],
+#   [11, 14, 14,  4, 19, 12, 15, 19,  0, 10, 18, 16, 16, 17, 15, 12, 13, 18, 13, 19, 11],
+#   [10, 19, 12,  8, 15,  9, 20, 18, 16,  0, 19, 20, 17, 19, 18, 12, 18, 18,  7, 14, 11],
+#   [17, 10, 12, 17, 15, 19, 14, 14, 19, 17,  0,  8, 13, 10,  9, 15, 14, 15, 12, 12, 14],
+#   [16,  9, 15, 12,  7, 18, 20, 10, 20, 14, 16,  0,  9, 13, 11, 10, 13,  9, 12,  8, 10],
+#   [19, 17, 15, 17, 12, 10, 17, 11, 16, 16, 17, 10,  0, 12, 18, 18, 17, 11,  7,  8, 15],
+#   [20, 12, 19, 11, 11, 15, 12, 18, 15, 19, 14, 12, 18,  0, 13, 19, 17, 14, 15, 13, 16],
+#   [14, 12, 11,  9, 18, 16, 18, 16, 20,  8, 16, 20, 15, 17,  0,  7,  9, 18, 12, 11, 18],
+#   [12, 13, 17,  8, 19, 12, 13, 19, 18,  9, 14, 19, 10, 13, 16,  0, 15, 16, 13, 15, 19],
+#   [13,  7, 13, 13,  9, 14, 12,  9, 14,  7, 12, 11, 14, 15, 10, 13,  0, 19,  9,  7,  9],
+#   [13, 12,  9, 15, 13, 14, 18, 13, 11, 10, 15, 14, 12, 20, 20, 10, 15,  0,  5,  6, 13],
+#   [16, 17, 19, 12, 18, 14, 16, 14, 18, 17, 12, 20, 13, 16, 19, 17, 15, 16,  0, 15, 10],
+#   [20, 13, 11, 10, 11, 14, 18,  9, 18, 15, 10, 16, 12, 20, 19, 11, 12, 19, 17,  0, 10],
+#   [17,  9, 16, 15, 14, 11, 14, 10, 12, 15,  9, 10,  8, 18, 12, 15, 16, 19, 12,  9,  0],
+#  ]
+
+def maxNumberItem(s, l):
+    max_l = max(l)
+    s_copy = s.copy()
+    s_copy.sort()
+    tmp = 0
+    i = 0
+    while(tmp+s_copy[i] < max_l):
+        tmp = tmp + s_copy[i]
+        i += 1
+    return i
 
 s = Solver() # create a solver s
 
@@ -105,6 +188,11 @@ def find(index,tours):
             list.append(And(sub_list))
 
     return list
+
+#constraint 1: each item exactly once
+for i in range(1,n+1): #iterate on 1,2,3...5
+    # for each [i][j] where bin(item_index) == [j][j][:] --> add constraint exactly_once
+    s.add(exactly_one_np(find(i,tours)))
 
 def binary_adder_2(a,b,name,solver):
     # effettuo soma binaria di a + b (che sono una lista[][] di True-false)
@@ -202,17 +290,7 @@ def binary_adder_(a,b,name,solver):
     return (d)
 
 
-# binary_adder_(sizes[0],sizes[1],"sus",s)
-# print(s.check())
-# model = s.model()
-# print(model)
 
-
-#constraint 1: each item exactly once
-for i in range(1,n+1): #iterate on 1,2,3...5
-    # for each [i][j] where bin(item_index) == [j][j][:] --> add constraint exactly_once
-    s.add(exactly_one(find(i,tours))) 
-    
 
 # constraint if there is the origin point, then all the number after it must be n+1
 # because he have terminated the journey
@@ -241,15 +319,20 @@ for i in range(m):
         tmp.append(tours[i][1][k])
     s.add(Or(tmp))
 
+
+# constraint to have the exact number of 0 in the matrix       
+bool_vars = [[tours[i][j] for j in range(n-m+3)] for i in range(m)]
+s.add(at_least_k_seq(bool_vars=find(0,tours), k=(n-m+3)*m-n, name="at_least_k_0"))
+
 # constraint for max courier load
 # sizes = [[Bool(f"size{i}_{j}") for j in range(depth_weight)] for i in range(m)]
 # capacities = [[Bool(f"capacity{i}_{j}") for j in range(depth_capacity)] for i in range(m)]
 # tours = [[[Bool(f"tour{i}_{j}_{k}") for k in range(depth_tours)] for j in range(n-m+3)] for i in range(m)]
         
-    """
+"""
     if(len_a != len_b):
         raise Exception("cardinality difference in binary_adder") 
-    """
+"""
     
 
 
@@ -333,8 +416,47 @@ def check_weight(solver):
         result=binary_subtraction(capacities[i],temp,f"courierSub{i}",solver)
         solver.add(result)
         
-        
 
+max_distance = max(max(D, key=lambda x: max(x))) #compute the maximum distance among all the distances
+depth_distance = math.ceil(math.log2(max_distance+1))
+distances = [[[Bool(f"distance{i}_{j}_{k}") for k in range(depth_distance)] for j in range(n-m+2)] for i in range(m)]
+def checkDistances(solver):
+    for i in range(n+1):
+        bin_enc1 = binary_encoding(i, depth_tours) #takes the encoding the number of the package
+        for j in range(n+1):
+            bin_enc2 = binary_encoding(j, depth_tours)
+            if i>0 and j>0:
+                distance = D[i-1][j-1]
+            elif i>0:
+                distance = D[i-1] [-1]
+            else:
+                distance = D[-1][j-1]
+            #print(distance)
+            bin_enc_distance = binary_encoding(distance, depth_distance)
+            
+            for x in range(m):
+                for y in range(n-m+2):
+                    bool_distance_enc = []
+                    for t in range(len(bin_enc_distance)): #doing the same thing on weights  
+                        if(bin_enc_distance[t] == "0"):   
+                            bool_distance_enc.append(Not(distances[x][y][t])) 
+                        else:
+                            bool_distance_enc.append(distances[x][y][t])
+
+                    bool_enc1 = []
+                    bool_enc2 = []
+                    for z in range(depth_tours):
+                        if bin_enc1[z] == '0':
+                            bool_enc1.append(Not(tours[x][y][z]))
+                        else:
+                            bool_enc1.append(tours[x][y][z])
+                    for z in range(depth_tours):
+                        if bin_enc2[z] == '0':
+                            bool_enc2.append(Not(tours[x][y+1][z]))
+                        else:
+                            bool_enc2.append(tours[x][y+1][z])
+                    
+                    solver.add(Implies(And(And(bool_enc1),And(bool_enc2)), And(bool_distance_enc)))
 
 def binary_subtractio2n(enc1,enc2,name,solver):
     len_a = len(enc1)
@@ -388,17 +510,20 @@ def binary_subtraction(enc1,enc2,name,solver):
     for i in range(delta):
         solver.add(Not(b[i]))
 
-    reversed_enc=[Bool(f"reverse_enc{name}_{i}") for i in range(len(b))]
-    for i in range(len(b)):
-        solver.add(Implies(b[i], Not(reversed_enc[i])))
-        solver.add(Implies(Not(b[i]), reversed_enc[i]))    
+    # reversed_enc=[Bool(f"reverse_enc{name}_{i}") for i in range(len(b))]
+    # for i in range(len(b)):
+    #     solver.add(Implies(b[i], Not(reversed_enc[i])))
+    #     solver.add(Implies(Not(b[i]), reversed_enc[i]))    
+    reversed_enc=[]
+    for i in b:
+        reversed_enc.append(Not(i))
     
     
     bool = [Bool(f"{name}_bit_to_add") for j in range(1)]
     solver.add(bool)
     complement2 = binary_adder_(reversed_enc,bool,f"{name}_temp",solver)
     # removing the bit added during the operation, because during the 2'complement operation we must mantein the same number of bits 
-    reversed_enc[len(complement2)-len(reversed_enc):]
+    complement2[len(complement2)-len(reversed_enc):]
     # for i in range(delta):
     #     solver.add(reversed_enc[i])
 
@@ -481,18 +606,19 @@ def printGivenName(model, nameGiven):
             print(f"{name} = {val}")
 
 check_weight(s)
-
+checkDistances(s)
 
 print(s.check())
 
-try:
-    model = s.model()
-    #printGivenName(model, "final")
-    printer(model,"weight",m,n-m+3,depth_weight)
-    printer(model,"tour",m,n-m+3,depth_tours)
-except:
-    pass
 
+model = s.model()
+f = open("demofile2.txt", "w")
+f.write(str(model))
+f.close()
+#printGivenName(model, "final")
+printer(model,"weight",m,n-m+3,depth_weight)
+printer(model,"tour",m,n-m+3,depth_tours)
+printer(model, "distance", m, n-m+2, depth_distance)
 
 """
 #tours = [[[Bool(f"tour{i}_{j}_{k}") for k in range(depth_tours)] for j in range(n-m+3)] for i in range(m)]
