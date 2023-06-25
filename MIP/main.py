5fad1bf8135d2d0bcaaf2ffce6902f1cef54891b
Giacomo <giacomo.caroli@live.it>
2023-06-25 17:04:54 +0200

    corretta assegnazione, imbastito calolo distance

--- MIP/main.py
+++ MIP/main.py
@@ -78,7 +78,17 @@ import numpy as np
 #calcolo distanza con interi
 #objective function con interi minimizza(minima somma)
 
+# Aggiungi il constraint "all different"
+#prob += lpSum(tours) == sum([i for i in range(n+1)])
+#lpSum(set(tuple(tours)))
 
+# uno = LpVariable("uno",lowBound=1,upBound=3,cat=LpInteger)
+# due = LpVariable("due",lowBound=1,upBound=3,cat=LpInteger)
+# tre = LpVariable("tre",lowBound=1,upBound=3,cat=LpInteger)
+
+# lista = [uno,due,tre]
+
+# prob += lpSum(lista) == 6
 
 
 #pesi = 
@@ -114,20 +124,55 @@ tours = [[[LpVariable(f"tour{i}_{j}_{k}",lowBound=0, upBound=1, cat=LpInteger) f
 #saltiamo il primo piano perchè è starting point (=0)
 for z in range(1,n+1):
     #vado a sommare tutto il piano
-    prob += lpSum([tours[i][j][z] for j in range(second_dimension) for i in range(m)])==1
+    prob += lpSum([tours[i][j][z] for j in range(second_dimension) for i in range(m)]) == 1
+
+#stesso vincolo ma sulla profondità
+for i in range(m):
+    for j in range(second_dimension):
+        prob += lpSum([tours[i][j][z] for z in range(n+1)]) == 1
+
+#sommatoria
+# i_00 * i_10 * d_00 +
+# i_00 * i_11 * d_01 +
+# i_00 * i_12 * d_02 +
+# i_00 * i_13 * d_03 +
+
+#itero per ogni corriere
+for c in range(m):
+
+    next_c = []
+
+    #itero per tutte le variabili i,j -> vista dall'alto del 3D, scorro prima la colonna e poi la riga
+    for i in range(second_dimension):
+        for j in range(n):
+            #recupero il valore della Lp variable corrispondente a i,j
+            current_i = tours[c][i][j]
+
+            #now itero sulla colonna successiva
+            
+            for jj in range(n):
+                if j==0 and jj==0:
+                    distance = D[n][n]
+                elif j==0 and jj!=0:
+                    distance = D[n][jj-1]
+                elif j!=0 and jj==0:
+                    distance = D[j-1][n]
+                else:
+                    distance = D[j-1][jj-1]
+
+                next_c.append(current_i * tours[c][i+1][jj] * distance)
+       
+    lpSum(next_c) #########QUESTO DA CONTINUARE !!!!!!!!
 
 
-# Aggiungi il constraint "all different"
-prob += lpSum(tours) == sum([i for i in range(n+1)])
-#lpSum(set(tuple(tours)))
+#variabile per objective function (minimizziamo la riga più grande)
+maxTravel = LpVariable("maxTravel",lowBound=0,upBound=None,cat=LpInteger)
 
-uno = LpVariable("uno",lowBound=1,upBound=3,cat=LpInteger)
-due = LpVariable("due",lowBound=1,upBound=3,cat=LpInteger)
-tre = LpVariable("tre",lowBound=1,upBound=3,cat=LpInteger)
+#aggiungo il vincolo per la maxTravel
+#prob += lpSum(#sommatoria) == maxTravel
 
-lista = [uno,due,tre]
+prob.setObjective()
 
-prob += lpSum(lista) == 6
 
 # Risolvi il problema
 prob.solve()
@@ -143,15 +188,15 @@ for var in prob.variables():
     
 
 
-# depthSearch=np.zeros((m,second_dimension))
+depthSearch=np.zeros((m,second_dimension))
 
-# for row in range(m):
-#     for column in range(second_dimension):
-#         temp=0
-#         for var in prob.variables():
-#             if f"tour{row}_{column}" in var.name:
-#                 if var.varValue==1.0:
-#                     temp=int(var.name.split("_")[-1])                
-#         depthSearch[row][column]=temp
+for row in range(m): #0:3
+     for column in range(second_dimension): #0:7
+         temp=0
+         for var in prob.variables():
+             if f"tour{row}_{column}" in var.name:
+                 if var.varValue==1.0:
+                     temp=int(var.name.split("_")[-1])                
+         depthSearch[row][column]=temp
     
-# print(depthSearch)
+print(depthSearch)
